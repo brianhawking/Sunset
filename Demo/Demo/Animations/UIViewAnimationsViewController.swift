e8a60ed4be57a033d076398876997fd71696635b
Brian Veitch <beveitch@gmail.com>
2023-01-04 18:43:40 -0600

    updating...

--- Demo/Demo/Animations/UIViewAnimationsViewController.swift
+++ Demo/Demo/Animations/UIViewAnimationsViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 import UIKit
-import SunsetFramework
+import Sunset
 
 class UIViewAnimationsViewController: UIViewController {
     
--- Demo/Pods/Target Support Files/SunsetFramework/SunsetFramework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SunsetFramework : NSObject
-@end
-@implementation PodsDummy_SunsetFramework
-@end
